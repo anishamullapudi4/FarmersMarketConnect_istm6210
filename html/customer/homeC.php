e67a92146a1457ad2bb8f6ebc30883f9ba8a5501
Anisha Mullapudi <79229798+anishamullapudi4@users.noreply.github.com>
2022-11-28 15:26:37 -0500

    Search file added

--- html/customer/homeC.php
+++ html/customer/homeC.php
@@ -23,17 +23,17 @@
     <div class="slideshow-container">
       <div class="mySlidesAncmnt">
         <div class="numberstext">1 / 3</div>
-        <img src="/images/ancmnt1.jpg" style="width:100%;" alt="Announcement1">
+        <img src="/imagesC/ancmnt1.jpg" style="width:100%;" alt="Announcement1">
         <div class="text">Announcement 1 Title</div>
       </div>
       <div class="mySlidesAncmnt">
         <div class="numberstext">2 / 3</div>
-        <img src="/images/ancmnt2.jpg" style="width:100%;" alt="Announcement2">
+        <img src="/imagesC/ancmnt2.jpg" style="width:100%;" alt="Announcement2">
         <div class="text">Announcement 2 Title</div>
       </div>
       <div class="mySlidesAncmnt">
         <div class="numberstext">3 / 3</div>
-        <img src="/images/ancmnt3.jpg" style="width:100%;" alt="Announcement3">
+        <img src="/imagesC/ancmnt3.jpg" style="width:100%;" alt="Announcement3">
         <div class="text">Announcement 3 Title</div>
       </div>
       <a class="prev" onclick="plusSlidesA(-1)">&#10094;</a>
@@ -60,10 +60,10 @@
           <th>@MattyMatt</th>
         </tr>
         <tr>
-          <td><img src="images/vendor1.jpg" class="vendorImg"></td>
-          <td><img src="images/vendor2.jpg" class="vendorImg"></td>
-          <td><img src="images/vendor3.jpg" class="vendorImg"></td>
-          <td><img src="images/vendor4.jpg" class="vendorImg"></td>
+          <td><img src="imagesC/vendor1.jpg" class="vendorImg"></td>
+          <td><img src="imagesC/vendor2.jpg" class="vendorImg"></td>
+          <td><img src="imagesC/vendor3.jpg" class="vendorImg"></td>
+          <td><img src="imagesC/vendor4.jpg" class="vendorImg"></td>
         </tr>
       </table>
   </div>
@@ -76,32 +76,32 @@
         <tr>
           <td>
             <h4>Freshfarm Dupont Circle Market</h4></br>
-            <h6>The Dupont Circle farmers market, one of the area’s most popular, offers something for everyone. Stop at one of the numerous farm stands to pick up herbs, vegetables, fruit, and free-range eggs. Several local creameries sell sheep and goat’s milk cheese and yogurt. Vegan soup, pancakes, wood-fired pizza, and bagels from a celebrated local deli make it a good bet for lunch. Beverage makers sell locally made cider, beer, liqueurs, and spirits during market hours, Sundays from 8:30 a.m. to 1:30 p.m.</h6> </td>
-            <td><img src="images/market1.jpg" width= "700" height="300" alt="Market" /></td>
+            <h6>The Dupont Circle farmers market, one of the area’s most popular, offers something for everyone. Stop at one of the numerous farm stands to pick up herbs, vegetables, fruit, and free-range eggs. Several local creameries sell sheep and goat’s milk cheese and yogurt. Vegan soup, pancakes, wood-fired pizza, and bagels from a celebrated local deli make it a good bet for lunch. Beverage makers sell locally made cider, beer, liqueurs, and spirits during market hours, Sundays from 8:30 a.m. to 1:30 p.m.<h6> </td>
+            <td><img src="imagesC/market1.jpg" width= "700" height="300" alt="Market" /></td>
           </tr>
           <tr>
             <td>
               <h4>Mount Pleasant Farmers Market</h4></br>
-                </h6>Live music, book sales, and a bike clinic animate this Mt. Pleasant market, open 9 a.m. to 1 p.m. on Saturdays. Farmers, bakers, and pickle makers from the mid-Atlantic sell everything from baby greens to grass-fed beef, kimchi, mint, and basil. </h6> </td>
-              <td><img src="images/market2.jpg" width= "700" height="300" alt="Market" /></td>
+            </h6>Live music, book sales, and a bike clinic animate this Mt. Pleasant market, open 9 a.m. to 1 p.m. on Saturdays. Farmers, bakers, and pickle makers from the mid-Atlantic sell everything from baby greens to grass-fed beef, kimchi, mint, and basil. <h6> </td>
+              <td><img src="imagesC/market2.jpg" width= "700" height="300" alt="Market" /></td>
             </tr>
             <tr>
               <td>
                 <h4>Columbia Heights Farmers Market</h4></br>
                 <h6>Handmade pasta, plant-based sausages, microgreens, and Eastern Shore mushrooms make this market a favorite among vegetarians. The market hosts 20 farmers and food producers Saturday mornings from 9 a.m. to 1 p.m. in the colorful Civic Plaza. </h6> </td>
-                <td><img src="images/market3.jpg" width= "700" height="300" alt="Market" /></td>
+                <td><img src="imagesC/market3.jpg" width= "700" height="300" alt="Market" /></td>
               </tr>
               <tr>
                 <td>
                   <h4>Silver Spring Farmers Market</h4></br>
                   <h6>Meat and dairy lovers will enjoy this farmers market, which sells pasture-raised meat, poultry, and eggs, as well as goat, cow, and sheep’s milk cheeses and yogurts. Beets, kale, and kohlrabi are among the featured produce. After food shopping, pick up some beeswax candles, soaps, and lotions and sip some apple and hard cider, or Maryland-produced wine and beer. It’s open Saturdays from 9 a.m. to 1 p.m.</h6> </td>
-                  <td><img src="images/market4.jpg" width= "700" height="300" alt="Market" /></td>
+                  <td><img src="imagesC/market4.jpg" width= "700" height="300" alt="Market" /></td>
                 </tr>
                 <tr>
                   <td>
                     <h4>Freshfarm Penn Quater Market</h4></br>
                     <h6>Located next to the Smithsonian National Portrait Gallery, the market features wood-fired pizza from Timber Pizza, scones and brownies from Rare Opportunity Farm and Bakehouse, and other mouthwatering fare. Fall produce finds include spring onions, radishes, broccoli, and fresh herbs like parsley, sage, and thyme. Get these items and more on Thursdays from 3 to 7 p.m. </h6> </td>
-                    <td><img src="images/market5.jpg" width= "700" height="300" alt="Market" /></td>
+                    <td><img src="imagesC/market5.jpg" width= "700" height="300" alt="Market" /></td>
                   </tr>
                 </div>
 
