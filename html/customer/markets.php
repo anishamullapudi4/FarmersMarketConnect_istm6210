30b750ad9eac6c6b515f3d9323f3ca4c1cb756a2
Anisha Mullapudi <79229798+anishamullapudi4@users.noreply.github.com>
2022-11-27 22:19:50 -0500

    Add files via upload

---
+++ html/customer/markets.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Farmers Market Connect</title>
+  <link rel="stylesheet" type="text/css" href="stylesV.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="javascript.js"></script>
+</head>
+  <body>
+    <div class="heading">
+      <?php include '../includes/headV.inc.php';  ?>
+      <?php include '../includes/navV.inc.php'; ?>
+    </div>
