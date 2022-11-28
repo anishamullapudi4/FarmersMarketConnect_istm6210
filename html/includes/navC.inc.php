2b260565f9bd0534e07aa762704d45ac1d200626
Anisha Mullapudi <79229798+anishamullapudi4@users.noreply.github.com>
2022-11-27 22:21:59 -0500

    Add files via upload

---
+++ html/includes/navC.inc.php
@@ -0,0 +1,7 @@
+<div class="navbar d-flex flex-row bd-highlight mb-3" id="vendorNav">
+  <div class="p-2 bd-highlight" id="homelink"><a href="homeV.php">Home</a></div>
+  <div class="p-2 bd-highlight"><a href="productsV.php">Search</a></div>
+  <div class="p-2 bd-highlight"><a href="inventoryTracking.php">Markets</a></div>
+  <div class="p-2 bd-highlight"><a href="homeV.php#followers">Vendors</a></div>
+  <div class="p-2 bd-highlight"><a href="homeV.php#reports">Products</a></div>
+</div>
